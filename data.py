79c0daddea76dfcf2fa17668f11ebee88b8a2859
tS6wG#!#wABQ632 <erwann-marobin@hotmail.fr>
2023-12-10 17:18:15 +0100

    test push

--- data.py
+++ data.py
@@ -2,7 +2,8 @@ characters = {
     'boxeur': {'vie': 100, 'force': 30},
     'voleur': {'vie': 80, 'force': 40},
     'gros': {'vie': 120, 'force': 20},
-    'gangster': {'vie': 75, 'force': 50}
+    'gangster': {'vie': 75, 'force': 50},
+    'test': {'vie': 100, 'force': 50}
 }
 
 adversaires = {
